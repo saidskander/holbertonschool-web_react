712125488792897821745212f919898432cf5d9b
saidskander <askander199h@outlook.com>
2022-02-24 10:39:58 +0200

    Done

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+  firstName: string
+  lastName: string
+  age: number
+  location: string
+}
+
+const student1: Student = {
+  firstName: "WeldMhamed",
+  lastName: "Mhamed",
+  age: 7,
+  location: "inest",
+};
+
+const student2: Student = {
+  firstName: "Skander",
+  lastName: "Amr",
+  age: 44,
+  location: "Sousse",
+};
